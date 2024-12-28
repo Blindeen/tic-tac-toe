7ada7c1433057e4f03afaef46604fdd3072b7982
Blindeen <kuba.szmajda@gmail.com>
2024-12-28 19:03:18 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
-import { join, resolve as _resolve } from "path";
+const path = require("path");
 
-export const mode = "development";
-export const entry = "./src/app.ts";
-export const devServer = {
-  static: [
-    {
-      directory: join(__dirname),
-    },
-  ],
-};
-export const output = {
-  filename: "app.js",
-  path: _resolve(__dirname, "out"),
-  publicPath: "/out/",
-};
-export const devtool = "inline-source-map";
-export const module = {
-  rules: [
-    {
-      test: /\.ts$/,
-      use: "ts-loader",
-      exclude: /node_modules/,
-    },
-  ],
-};
-export const resolve = {
-  extensions: [".ts", ".js"],
+module.exports = {
+  mode: "development",
+  entry: "./src/app.ts",
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "out"),
+    publicPath: "/out/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
 };
