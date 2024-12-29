a4e3a406d0dc2dc8fa5e1109972a3dd572ddef58
Blindeen <kuba.szmajda@gmail.com>
2024-12-29 20:45:21 +0100

    attach onclick event to cell element

--- src/components/cell-object.ts
+++ src/components/cell-object.ts
@@ -1,12 +1,30 @@
 import { Cell } from "../models/cell";
+import { AutoBind } from "../utilities/autobind";
 
 class CellObject {
   private element: HTMLDivElement;
   private cell: Cell;
 
-  constructor(element: HTMLDivElement, cell: Cell) {
+  constructor(element: HTMLDivElement, id: number) {
     this.element = element;
-    this.cell = cell;
+    this.cell = new Cell(id);
+    this.configure();
+  }
+
+  private configure() {
+    this.element.addEventListener("click", this.onCellClick);
+  }
+
+  @AutoBind
+  private onCellClick(event: Event) {
+    const shapeTemplate = document.getElementById(
+      "circle-template"
+    )! as HTMLTemplateElement;
+    const newShape = document.importNode(
+      shapeTemplate.content.firstElementChild!,
+      true
+    );
+    this.element.appendChild(newShape);
   }
 }
 
